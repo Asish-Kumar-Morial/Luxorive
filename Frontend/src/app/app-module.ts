8a70308ddd44960d1c7463b016b9a666bbb32614
Asish Kumar Morial <asishmorial@gmail.com>
2025-07-09 00:49:26 +0530

    Re-added Frontend folder after cleaning internal

---
+++ Frontend/src/app/app-module.ts
@@ -0,0 +1,34 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { provideNzI18n } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideNzI18n(en_US),
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
